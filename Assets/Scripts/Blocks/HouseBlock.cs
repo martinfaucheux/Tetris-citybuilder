a2deb6e611b7d299aa4641898658d80cdb535e9c
Martin Faucheux <martin.faucheux@gmail.com>
2023-09-28 18:02:50 +0200

    add satisfaction aura

--- Assets/Scripts/Blocks/HouseBlock.cs
+++ Assets/Scripts/Blocks/HouseBlock.cs
@@ -6,7 +6,12 @@ public class HouseBlock : Block
 {
     public int peopleAmount = 3;
     public int taxPerPeople = 1;
-    public override ResourceGroup GetProduct() => new ResourceGroup(people: peopleAmount);
+
+    public override ResourceGroup GetProduct()
+    {
+        int peopleYield = peopleAmount + SatisfactionManager.instance.GetSatisfaction(matrixCollider.matrixPosition);
+        return new ResourceGroup(people: peopleYield);
+    }
 
     public override ResourceGroup GetPermanentProduct() => new ResourceGroup(gold: -peopleAmount * taxPerPeople);
 }
--- Assets/Scripts/SatisfactionManager.cs
+++ Assets/Scripts/SatisfactionManager.cs
@@ -2,8 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SatisfactionManager : MonoBehaviour
+public class SatisfactionManager : Singleton<SatisfactionManager>
 {
-    // Grid to keep track of the satisfaction of each tile
+    public GenericGrid<int> satisfactionGrid = new GenericGrid<int>();
 
+    public void ComputeGrid()
+    {
+        foreach (Block block in BlockManager.instance.blockList)
+        {
+            foreach (KeyValuePair<Vector2Int, int> pair in block.GetSatisfactionAura())
+            {
+                Vector2Int pos = pair.Key + block.matrixCollider.matrixPosition;
+                satisfactionGrid[pos] += pair.Value;
+            }
+        }
+    }
+
+    public int GetSatisfaction(Vector2Int pos)
+    {
+        if (satisfactionGrid.ContainsKey(pos))
+            return satisfactionGrid[pos];
+        return 0;
+    }
 }
