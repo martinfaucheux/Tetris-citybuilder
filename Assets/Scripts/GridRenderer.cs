95d273cc4c6b812a844a92137d254ccc810e1b5a
Martin Faucheux <martin.faucheux@gmail.com>
2023-09-30 19:33:23 +0200

    reorganize files

--- Assets/Scripts/GridRenderer.cs
+++ Assets/Scripts/GridRenderer.cs
@@ -7,15 +7,15 @@ public class GridRenderer : MonoBehaviour
 {
     public int maxY = 1000;
     public GameObject gridCellPrefab;
-    CollisionMatrix matrix { get => CollisionMatrix.instance; }
+    BlockContextManager contextManager { get => BlockContextManager.instance; }
 
     void Start()
     {
-        for(int x = -matrix.maxX; x <= matrix.maxX; x++)
+        for (int x = -contextManager.currentContext.xMax; x <= contextManager.currentContext.xMax; x++)
         {
-            for(int y = 0; y < maxY; y++)
+            for (int y = 0; y < maxY; y++)
             {
-                Vector3 position = matrix.GetRealWorldPosition(new Vector2Int(x, y));
+                Vector3 position = contextManager.GetRealWorldPosition(new Vector2Int(x, y));
                 position.z = 1f;
                 GameObject gridCellObj = Instantiate(gridCellPrefab, position, Quaternion.identity, transform);
                 TextMeshPro text = gridCellObj.GetComponentInChildren<TextMeshPro>();
